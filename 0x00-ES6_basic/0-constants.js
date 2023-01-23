b26bd32d74b51a5ec131f0ccf005c83b0660d81d
Franklin Obasi <franklinobasy@gmail.com>
2023-01-23 11:47:05 +0100

    Task 0: 0. Const or let?

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -10,6 +10,5 @@ export function getLast() {
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,2 @@
 import { taskFirst, taskNext } from './0-constants.js';
-
 console.log(`${taskFirst()} ${taskNext()}`);
