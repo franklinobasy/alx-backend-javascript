762d7fbecc5399529e1d88ca1dad32fcaf089a8d
Franklin Obasi <franklinobasy@gmail.com>
2023-01-23 11:51:53 +0100

    Task 3: Parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -5,5 +5,5 @@
 // export default getSumOfHoods;
 
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
-    return initialNumber + expansion1989 + expansion2019;
+  return initialNumber + expansion1989 + expansion2019;
 }
