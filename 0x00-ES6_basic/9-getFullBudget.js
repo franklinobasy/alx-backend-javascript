e3b2e01c7736072792e9e9b28d26be8a737e9041
Franklin Obasi <franklinobasy@gmail.com>
2023-01-23 13:45:15 +0100

    Task 9: ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,10 +1,10 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars (income) {
+    getIncomeInDollars(income) {
       return `$${income}`;
     },
     getIncomeInEuros(income) {
--- 0x00-ES6_basic/9-main.js
+++ 0x00-ES6_basic/9-main.js
@@ -2,7 +2,5 @@ import getFullBudgetObject from './9-getFullBudget.js';
 
 const fullBudget = getFullBudgetObject(20, 50, 10);
 
-console.log(fullBudget);
-console.log(fullBudget.income);
 console.log(fullBudget.getIncomeInDollars(fullBudget.income));
 console.log(fullBudget.getIncomeInEuros(fullBudget.income));
