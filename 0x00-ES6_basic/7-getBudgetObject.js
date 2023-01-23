d4c7ab14c462372a04d2d13fd252a702d2a60960
Franklin Obasi <franklinobasy@gmail.com>
2023-01-23 15:03:39 +0100

    Task 7: Object property value shorthand syntax

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
   return budget;
 }
