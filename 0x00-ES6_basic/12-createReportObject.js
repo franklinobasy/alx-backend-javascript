e146f79b12701685a019e57c1ddfc46eb1783559
Franklin Obasi <franklinobasy@gmail.com>
2023-01-23 15:09:59 +0100

    Task 11: Iterator

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments(list) {
-      return Array.from(list).length;
+      return Object.keys(list).length;
     },
   };
 }
