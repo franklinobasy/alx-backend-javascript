71d31f8d05cf33c4a4eead9b9e0c55b8771573f9
Obasi Franklin <franklinobasy@gmail.com>
2023-01-25 10:00:31 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
-    }).catch(() => new Error());
+    }).catch(() => new Error())
+      .finally(() => console.log('Got a response from the API'));
 }
