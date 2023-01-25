3c6e5eead4e6bc791cd87566c1d5f922b31a67fd
Obasi Franklin <franklinobasy@gmail.com>
2023-01-25 10:43:28 +0100

    Task 3: Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return uploadPhoto()
-          .then((res1) => {
-            createUser()
-              .then((res2) => {
-                console.log(
-                  `${res1.body} ${res2.firstName} ${res2.lastName}`,
-                );
-              })
-              .catch(() => console.log('Signup system offline'));
-          }).catch(() => console.log('Signup system offline'));
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
