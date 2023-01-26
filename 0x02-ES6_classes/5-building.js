d3bfba3221cd3da316c0a965e340ba3ece977de8
Franklin Obasi <franklinobasy@gmail.com>
2023-01-26 19:09:59 +0100

    Task 5: A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,12 @@
 class Building {
   constructor(sqft) {
-    if (
-      this.constructor === Building
-      && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
-
-    this.sqft = sqft;
   }
 
   /**
