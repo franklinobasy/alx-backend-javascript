1e0061ac77bdfe19427aa945a65be25310277538
Franklin Obasi <franklinobasy@gmail.com>
2023-01-26 19:38:47 +0100

    Task 8:  Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,19 +3,29 @@ import Building from './5-building';
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
-  get sqft() {
-    return this._sqft;
+  /**
+   * @param {Number} floors
+   */
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
+  get sqft() {
+    return this._sqft;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return `Evacuate slowly the ${this.floor} floors`;
   }
 }
 
