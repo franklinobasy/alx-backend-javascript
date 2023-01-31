74645931cead6ed4c875059a9295525c383c82f3
Franklin Obasi <franklinobasy@gmail.com>
2023-01-31 10:55:57 +0100

    Task 10: More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,10 @@
 export default function updateUniqueItems(map) {
-  if (!map || !(map instanceof Map)) {
-    return new Error('Cannot process');
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
-
-  for (const [key, value] of map) {
+  map.forEach((value, key) => {
     if (value === 1) {
       map.set(key, 100);
     }
-  }
+  });
 }
