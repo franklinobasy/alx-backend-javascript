18f359425576ce720cf5d7e8f337c89476874921
Franklin Obasi <franklinobasy@gmail.com>
2023-02-01 23:54:34 +0100

    Task 5:  Advanced types Part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,46 @@
+export interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+export interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+export class Director implements DirectorInterface {
+  workFromHome() {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak() {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks() {
+    return 'Getting to director tasks';
+  }
+}
+
+export class Teacher implements TeacherInterface {
+  workFromHome() {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak() {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks() {
+    return 'Getting to work';
+  }
+}
+
+export function createEmployee(salary: (number | string)): (Director | Teacher) {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
